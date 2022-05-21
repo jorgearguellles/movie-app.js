8bfa0e16831ce378559bb15c6cc9ed6a5c6d79f3
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-05-21 11:13:43 -0500

    add/ getMovieById() done

--- src/main.js
+++ src/main.js
@@ -97,7 +97,19 @@ const getTrendingMovies = async () => {
 const getMovieById = async (id) => {
   const { data:movie } = await api('movie/' + id);
 
+  const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+  headerSection.style.background = `
+    linear-gradient(
+      180deg, 
+      rgba(0, 0, 0, 0.35) 19.27%, 
+      rgba(0, 0, 0, 0) 29.17%
+    ),
+    url(${movieImgUrl})  
+  `;
+
   movieDetailTitle.textContent = movie.title;
   movieDetailDescription.textContent = movie.overview;
   movieDetailScore.textContent = movie.vote_average;
+
+  createCategories(movie.genres, movieDetailCategoriesList);
 };
